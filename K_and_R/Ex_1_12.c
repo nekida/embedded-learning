667cd5309752e37837023a2bfd7d4eabdecc8cb7
nekida <nekida@gmail.com>
2026-09-01 07:03:41 +0000

    Changes Ex_1_12.c

--- K_and_R/Ex_1_12.c
+++ K_and_R/Ex_1_12.c
@@ -5,23 +5,29 @@
 
 int main (void)
 {
-    int c, state;
+    int c, state, cnt_symbols;
 
     state = OUT;
+    cnt_symbols = 0;
 
     while ((c = getchar()) != EOF) {    
-        if (c == ' ' || c == '\n' || c == '\t')
+        if (c == ' ' || c == '\n' || c == '\t') {
             state = OUT;
-        else if (state == OUT) {
+            ++cnt_symbols;
+        } else if (state == OUT) {
             state = IN;
         }
 
         if (state == IN) {
+            if (cnt_symbols != 0) {
+                cnt_symbols = 0;
+                putchar('\n');
+            }
             putchar(c);
-        } else {
-            putchar('\n');
         }
     }
+
+    putchar('\n');
 
     return 0;
 }
