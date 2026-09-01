90d7cce18a62a489089847e413f45f2dfa557895
nekida <nekida@gmail.com>
2026-09-01 10:50:05 +0000

    Add Ex_1_13.c. Not OK

---
+++ K_and_R/Ex_1_13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main (void)
+{
+    int c, state, cnt_chars, histogram[10];
+
+    state = OUT;
+    cnt_chars = 0;
+    for (int i = 0; i < 10; ++i)
+        histogram[i] = 0;
+
+    while ((c = getchar()) != EOF) {    
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        } else {
+            ++cnt_chars;
+            state = IN;
+        }
+        
+        if (state == OUT && cnt_chars != 0) {
+            ++histogram[cnt_chars];
+            cnt_chars = 0;
+        }
+
+    }
+
+    for (int i = 0; i < 10; ++i)
+        printf ("%d ", histogram[i]);
+
+    putchar('\n');
+
+    return 0;
+}
